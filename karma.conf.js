aa07687b26abf6eaacbb2d6ea3f322b25a1afa38
mycloudops <65656585+mycloudops@users.noreply.github.com>
2020-09-15 15:54:40 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,9 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    files: [
+    'node_modules/es6-shim/es6-shim.js'
+    ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
