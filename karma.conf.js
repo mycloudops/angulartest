725bac020cc978e59e189d164bd0fbb322668256
mycloudops <65656585+mycloudops@users.noreply.github.com>
2020-09-15 15:23:39 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,17 +25,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--headless','--disable-gpu',
-          '--no-sandbox'
-        ]
-     }
-    },
-    browsers: ['ChromeHeadless'],
-    singleRun: false,
+    browsers: ['PhantomJS'],
+    singleRun: true,
     restartOnFileChange: true
   });
 };
